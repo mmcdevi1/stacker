234cf4db590f12e188fd3dc12aaa29354b69e5ee
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-22 21:03:53 -0400

    create user controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'register', to: "users#new"
+  resources :users, only: [:create]
+
+  root to: "users#new"
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+describe UsersController do
+  describe "GET new" do 
+    it "sets @user variable" do 
+      get :new
+      expect(assigns(:user)).to be_instance_of(User)
+    end
+  end
 
+  describe "POST create" do 
+    context "with valid inputs" do 
+      before do 
+        post :create, user: Fabricate.attributes_for(:user)
+      end
+
+      it "creates the user" do 
+        expect(User.count).to eq(1)
+      end
+
+      it "redirects to the root path" do 
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "with invalid inputs" do 
+      before do 
+        post :create, user: { password: "password", full_name: "Mike mcdevitt" }
+      end
+
+      it "does not create the user" do 
+        expect(User.count).to eq(0)
+      end
+
+      it "renders the new template" do 
+        expect(response).to render_template 'new'
+      end
+
+      it "sets @user" do 
+        expect(assigns(:user)).to be_instance_of(User)
+      end
+    end
+  end
 end
